e7232d0f68c17bf685298c99b91a430a0075e4c5
Zachary Lipscomb <zachlipscomb86@gmail.com>
2023-02-26 13:02:06 -0800

    Working on bidding site, learn to connect to database is next step

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import socketIO from 'socket.io-client';
+import Home from './components/Home'
+import Products from './components/Products'
+import AddProduct from './components/Addproduct'
+import BidProduct from './components/BidProduct'
+import Nav from './components/Nav'
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+
+//socket library to be passed and utilized 
+const socket = socketIO.connect('http://localhost:4000');
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav socket={socket} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/add" element={<AddProduct socket={socket} />} />
+          <Route
+            path="/products/bid/:name/:price"
+            element={<BidProduct socket={socket} />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
